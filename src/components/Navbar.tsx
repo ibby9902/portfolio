445c2487b235205d7323580db1148f6068449b6a
Ibby <ibby364@outlook.com>
2022-06-10 04:07:38 +0100

    Add Navbar & styles

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="Navbar">
+      <ul className="Navbar-navList">
+        <li className="nav-link"><a href="www.google.com">Home</a></li>
+        <li className="nav-link"><a href="www.google.com">Projects</a></li>
+      </ul>
+      
+    </div>
+  )
+}
+
+export default Navbar
