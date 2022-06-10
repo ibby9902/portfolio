a1e6bf53a4cd1c2f0b6dfef36f8f8ab8e25e919d
Ibby <ibby364@outlook.com>
2022-06-10 04:08:01 +0100

    Added Intro component

---
+++ src/components/Intro.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Intro.css';
+const Intro = () => {
+  return (
+        <div className="intro-container">
+            <div className="intro-content">
+                <h1>Ibby</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga perspiciatis neque porro nemo molestiae soluta culpa quod odio necessitatibus quaerat hic voluptas praesentium aspernatur ad eius, laboriosam recusandae sed modi.</p>
+            </div>
+        </div>
+  )
+}
+
+export default Intro
