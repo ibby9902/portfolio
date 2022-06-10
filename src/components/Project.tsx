aba54174d83e22548076766a8600af425ae63a2b
Ibby <ibby364@outlook.com>
2022-06-10 04:08:38 +0100

    Add Project component

---
+++ src/components/Project.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Project.css'
+
+const Project = () => {
+  return (
+    <div className="project-container">
+      <div className="project-img-container"></div>
+      <h3 className="project-title">Mobile App</h3>
+      <p className="project-description">Project Description here</p>
+      <div className="project-technologies"></div>
+      <div className="project-buttons"></div>
+    </div>
+  )
+}
+
+export default Project
